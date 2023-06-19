a8582fb938dd131b31e553a1d08f5c31574cbeb9
Rezwan Ahmed <rezwan490@gmail.com>
2023-06-19 23:09:31 +0600

    Updated certificate validity years to 10.

--- generate-ip-cert.sh
+++ generate-ip-cert.sh
@@ -32,5 +32,5 @@ subjectAltName = @alt_names
 IP.1 = $IP
 " > san.cnf
 
-openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout key.pem -out cert.pem -config san.cnf
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout key.pem -out cert.pem -config san.cnf
 rm san.cnf
